bdc958e9a57207af2b28e69ed9adf9a133b05889
Boyko Alexey <elorian@yandex.ru>
2021-11-01 15:45:13 +0300

    2/12 done

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+url = 'https://api-ssl.bitly.com/v4/user'
+payload = {'Authorization': os.getenv('KEY')}
+response = requests.get(url, headers=payload).json()
+response.raise_for_status()
+print(response)
+
