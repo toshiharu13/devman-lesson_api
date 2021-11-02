671ae8b4e73e94170aab7bdecc1b43b4b9b4e171
elf <elorian@yandex.ru>
2021-11-02 14:38:28 +0300

    8/12 done

--- bitlink_create_link.py
+++ bitlink_create_link.py
@@ -12,9 +12,25 @@ def shorten_link(token, url, link_to_bitly):
     return response.json()['link']
 
 
-url = 'https://api-ssl.bitly.com/v4/bitlinks'
+def count_clicks(token, url):
+    headers = {'Authorization': token}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def check_for_bitly_link(link_to_check):
+    return 'bit.ly/' in link_to_check
+
+
+
+url_to_bitly = 'https://api-ssl.bitly.com/v4/bitlinks'
 key = os.getenv('KEY')
+
 link_to_bitly = input('Веедите адрес для битлинка: ')
-
-print(f'Битлинк: {shorten_link(key, url, link_to_bitly)}')
-
+if check_for_bitly_link(link_to_bitly):
+    url_count_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{link_to_bitly[7:]}/clicks/summary'
+    print(f'количество кликов по ссылке: {count_clicks(key, url_count_clicks)}')
+else:
+    response = shorten_link(key, url_to_bitly, link_to_bitly)
+    print(f'Битлинк: {response}')
