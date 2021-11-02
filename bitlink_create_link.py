d46c941c3ada46fbd2236744872ace8dd903fcbc
elf <elorian@yandex.ru>
2021-11-02 14:50:37 +0300

    10/12 done

--- bitlink_create_link.py
+++ bitlink_create_link.py
@@ -23,14 +23,14 @@ def check_for_bitly_link(link_to_check):
     return 'bit.ly/' in link_to_check
 
 
+if __name__ == "__main__":
+    url_to_bitly = 'https://api-ssl.bitly.com/v4/bitlinks'
+    key = os.getenv('KEY')
 
-url_to_bitly = 'https://api-ssl.bitly.com/v4/bitlinks'
-key = os.getenv('KEY')
-
-link_to_bitly = input('Веедите адрес для битлинка: ')
-if check_for_bitly_link(link_to_bitly):
-    url_count_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{link_to_bitly[7:]}/clicks/summary'
-    print(f'количество кликов по ссылке: {count_clicks(key, url_count_clicks)}')
-else:
-    response = shorten_link(key, url_to_bitly, link_to_bitly)
-    print(f'Битлинк: {response}')
+    link_to_bitly = input('Веедите адрес для битлинка: ')
+    if check_for_bitly_link(link_to_bitly):
+        url_count_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{link_to_bitly[7:]}/clicks/summary'
+        print(f'количество кликов по ссылке: {count_clicks(key, url_count_clicks)}')
+    else:
+        response = shorten_link(key, url_to_bitly, link_to_bitly)
+        print(f'Битлинк: {response}')
