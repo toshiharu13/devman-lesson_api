60bedf7635f57f047735ef5af55ec4bda6a42f70
elf <elorian@yandex.ru>
2021-11-03 15:24:54 +0300

    add error handler

--- bitlink_create_link.py
+++ bitlink_create_link.py
@@ -10,6 +10,9 @@ def shorten_link(token, url, link_to_bitly):
     check_for_correct_link = requests.get(link_to_bitly)
     check_for_correct_link.raise_for_status()
     response = requests.post(url, headers=headers, json=body)
+    response.raise_for_status()
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error'])
     return response.json()['link']
 
 
@@ -17,6 +20,8 @@ def count_clicks(token, url):
     headers = {'Authorization': token}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error'])
     return response.json()['total_clicks']
 
 
@@ -27,12 +32,15 @@ def check_for_bitly_link(link_to_check):
 if __name__ == "__main__":
     load_dotenv()
     url_to_bitly = 'https://api-ssl.bitly.com/v4/bitlinks'
-    key = os.getenv('KEY')
+    key = os.getenv('KEY_TO_BITLY')
 
     link_to_bitly = input('Веедите адрес для битлинка: ')
-    if check_for_bitly_link(link_to_bitly):
-        url_count_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{link_to_bitly[7:]}/clicks/summary'
-        print(f'количество кликов по ссылке: {count_clicks(key, url_count_clicks)}')
-    else:
-        response = shorten_link(key, url_to_bitly, link_to_bitly)
-        print(f'Битлинк: {response}')
+    try:
+        if check_for_bitly_link(link_to_bitly):
+            url_count_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{link_to_bitly[7:]}/clicks/summary'
+            print(f'количество кликов по ссылке: {count_clicks(key, url_count_clicks)}')
+        else:
+            response = shorten_link(key, url_to_bitly, link_to_bitly)
+            print(f'Битлинк: {response}')
+    except:
+        raise
--- temp.py
+++ temp.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 url = 'https://api-ssl.bitly.com/v4/user'
-payload = {'Authorization': os.getenv('KEY')}
+payload = {'Authorization': os.getenv('KEY_TO_BITLY')}
 response = requests.get(url, headers=payload).json()
 response.raise_for_status()
 print(response)
